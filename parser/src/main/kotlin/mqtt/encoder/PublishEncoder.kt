3ff5787323551b0e3b3fb0bd1c20e2b2f05f6730
JKamue <mail@jkamue.de>
2025-10-06 19:12:11 +0200

    perf: dont copy payload for publishing

--- parser/src/main/kotlin/mqtt/encoder/PublishEncoder.kt
+++ parser/src/main/kotlin/mqtt/encoder/PublishEncoder.kt
@@ -6,11 +6,7 @@ import java.nio.ByteBuffer
 object PublishEncoder {
     fun encodeScatter(packet: PublishPacket): Array<ByteBuffer> {
         val topicName = MqttEncoderHelpers.encodeUtf8StringUtf8Bytes(packet.topic.topic)
-
-        val test = packet.payload.remaining()
-
         val payload = packet.payload.duplicate().rewind()
-        val test2 = payload.remaining()
 
         val packetLength =
             1 + // header
@@ -26,10 +22,9 @@ object PublishEncoder {
         buffer.put(topicName)
         // Qos 0 -> no identifier MqttEncoderHelpers.encodeTwoByteInt(packet.packetIdentifier, buffer)
         buffer.put(0b00000000.toByte())
-        buffer.put(payload)
 
         buffer.flip()
 
-        return arrayOf(buffer)
+        return arrayOf(buffer, payload)
     }
 }
